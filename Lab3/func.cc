f903b82af01e7dcac40d14680143a5d4706ce7b5
Chavez05 <78380052+Chavez05@users.noreply.github.com>
2021-02-22 15:18:25 -0500

    Update func.cc

--- Lab3/func.cc
+++ Lab3/func.cc
@@ -33,6 +33,35 @@ int sumsq(int a, int b) {
 }
 // 4
 bool isPrime(int a) {}
+
+//4 -Chavez Summerville 
+bool isPrime(int num);
+
+int main() {
+	int num;
+	bool flag;
+	cout << "Enter any number(should be positive integer): ";
+	cin >> num;
+	flag = isPrime(num);
+	if (flag == true)
+		cout << num << " is a prime number";
+	else
+		cout << num << " is not a prime number";
+	return 0;
+}
+
+bool isPrime(int num) {
+	bool flag = true;
+	for (int i = 2; i <= num / 2; i++) {
+		if (num % i == 0) {
+			flag = false;
+			break;
+		}
+	}
+	return flag;
+}
+
+
 // 5
 <<<<<<< HEAD
 int countPrimes(int a, int b) {
@@ -136,6 +165,26 @@ int countPrimes(int a, int b) {
 
   // 4
   int prod(int x[], int length) {}
+  
+//4 -Chavez Summerville 
+
+int prod(int x[], int length) {
+
+	double product = 1;
+
+	for (int i = 0; i < length; i++)
+	{
+		product = product * x[i];
+	}
+	return product;
+}int prod(int x[], int length) {
+  int total=1;
+  for(int i=0; i<length; i++){
+    total=total*x[i];
+  }
+  return total;
+}
+
 
 <<<<<<< HEAD
   // 5
